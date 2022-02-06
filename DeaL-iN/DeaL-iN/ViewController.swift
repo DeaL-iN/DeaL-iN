056f444beba7a8c6d803685cd9374e9d33d31019
songjucho <pinetree134@ajou.ac.kr>
2022-02-06 16:26:51 +0900

    [iOS] feat: 프로젝트 생성

---
+++ DeaL-iN/DeaL-iN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeaL-iN
+//
+//  Created by 송주 on 2022/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
