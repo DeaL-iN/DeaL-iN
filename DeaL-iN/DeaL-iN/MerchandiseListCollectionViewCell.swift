1b8e0569d3897e4d2dbfda548b4cab80e1080af3
songjucho <pinetree134@ajou.ac.kr>
2022-02-07 15:32:43 +0900

    [iOS] feat: 상품 목록 컬렉션뷰 셀 추가

---
+++ DeaL-iN/DeaL-iN/MerchandiseListCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MerchandiseListCollectionViewCell.swift
+//  DeaL-iN
+//
+//  Created by 송주 on 2022/02/07.
+//
+
+import UIKit
+
+class MerchandiseListCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "MerchandiseListCollectionViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
