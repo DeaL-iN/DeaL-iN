765e31948794aebfc0159d7c1a07582f2b4d691a
songjucho <pinetree134@ajou.ac.kr>
2022-02-07 16:51:59 +0900

    [iOS] feat: 상품 목록 컬렉션뷰 추가
    - 컬렉션뷰 사이즈 및 이미지 사이즈 조정 필요

--- DeaL-iN/DeaL-iN/MerchandiseListCollectionViewCell.swift
+++ DeaL-iN/DeaL-iN/MerchandiseListCollectionViewCell.swift
@@ -10,9 +10,19 @@ import UIKit
 class MerchandiseListCollectionViewCell: UICollectionViewCell {
     
     static let identifier: String = "MerchandiseListCollectionViewCell"
-
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var merchandiseNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    public func configure(image: UIImage, merchandiseName: String, price: String) {
+        merchandiseNameLabel?.text = merchandiseName
+        priceLabel?.text = price
+        thumbnailImageView.backgroundColor = UIColor(red: 100, green: 100, blue: 100, alpha: 1)
     }
 
 }
