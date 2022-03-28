51408126291b9c8dc6cabe858442f68a818ab3b6
songjucho <pinetree134@ajou.ac.kr>
2022-03-28 15:58:52 +0900

    [iOS] feat: Notices 모델 추가

---
+++ iOS/DeaL-iN/DeaL-iN/Model/Notices.swift
@@ -0,0 +1,15 @@
+//
+//  Notices.swift
+//  DeaL-iN
+//
+//  Created by 송주 on 2022/03/28.
+//
+
+import Foundation
+
+struct Notices: Hashable {
+    var id: Int
+    var title: String
+    var memo: String
+    var created_at: Date
+}
