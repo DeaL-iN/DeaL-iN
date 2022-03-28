7342a8eb347dc812de3cb6279684fae77c828241
songjucho <pinetree134@ajou.ac.kr>
2022-03-28 16:01:09 +0900

    [iOS] feat: Chat_room 모델 추가

---
+++ iOS/DeaL-iN/DeaL-iN/Model/Chat_room.swift
@@ -0,0 +1,14 @@
+//
+//  Chat_room.swift
+//  DeaL-iN
+//
+//  Created by 송주 on 2022/03/28.
+//
+
+import Foundation
+
+struct Chat_room: Hashable {
+    var id: Int
+    var item_id: Int
+    var created_at: Date
+}
