651d59bda37b38b1c84fc54e8c78be422253371e
songjucho <pinetree134@ajou.ac.kr>
2022-03-28 15:54:16 +0900

    [iOS] feat: Users 모델 생성

---
+++ iOS/DeaL-iN/DeaL-iN/Model/Users.swift
@@ -0,0 +1,26 @@
+//
+//  Users.swift
+//  DeaL-iN
+//
+//  Created by 송주 on 2022/03/28.
+//
+
+import Foundation
+
+struct Users: Hashable {
+    var id: Int
+    var email: String
+    var password: String
+    var name: String
+    var nickname: String
+    var tel: String
+    var address: String
+    var address_detail: String
+    var point: Int
+    var grade: String
+    var is_deleted: Bool
+    var profile_image_url: String
+    var blacklist: Bool
+    var created_at: Date
+    var updated_at: Date
+}
