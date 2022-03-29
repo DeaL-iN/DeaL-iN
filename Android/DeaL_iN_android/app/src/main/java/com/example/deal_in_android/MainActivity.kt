95e1087ef9361dc4fb36a2c359cd51558f08b806
haena3230 <gosk0912@naver.com>
2022-03-29 11:34:17 +0900

    [Android] add font style

--- Android/DeaL_iN_android/app/src/main/java/com/example/deal_in_android/MainActivity.kt
+++ Android/DeaL_iN_android/app/src/main/java/com/example/deal_in_android/MainActivity.kt
@@ -2,23 +2,19 @@ package com.example.deal_in_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import com.example.deal_in_android.R
 import com.example.deal_in_android.chat.ChatListFragment
 import com.example.deal_in_android.mypage.MyPageFragment
 import com.example.deal_in_android.mypage.users.LoginFragment
 import com.example.deal_in_android.post.PostFragment
 import com.example.deal_in_android.product.ProductListFragment
 import com.example.deal_in_android.search.SearchFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     // Bottom Navigation에서 활용될 5개 Fragment 우선 설정
-    private val productListFragment = ProductListFragment()
+    private val productListFragment =ProductListFragment()
     private val searchFragment = SearchFragment()
     private val postFragment = PostFragment()
     private val chatListFragment = ChatListFragment()
