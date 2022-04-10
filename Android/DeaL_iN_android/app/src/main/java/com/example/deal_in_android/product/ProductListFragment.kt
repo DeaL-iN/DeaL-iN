a1aedf4748d373d4fd0c51956dee16af9440395b
haena3230 <gosk0912@naver.com>
2022-04-10 23:45:50 +0900

    [Android] add best image box in product_list_page

--- Android/DeaL_iN_android/app/src/main/java/com/example/deal_in_android/product/ProductListFragment.kt
+++ Android/DeaL_iN_android/app/src/main/java/com/example/deal_in_android/product/ProductListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputBinding
 import com.example.deal_in_android.R
 
 class ProductListFragment : Fragment() {
@@ -13,7 +14,9 @@ class ProductListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         return inflater.inflate(R.layout.fragment_product_list, container, false)
+
     }
 }
